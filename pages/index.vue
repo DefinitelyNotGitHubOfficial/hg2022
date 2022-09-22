18a24dd0e4cb129def72615a05fd9f0a769afa84
Damien <34070001+DefinitelyNotGitHubOfficial@users.noreply.github.com>
2022-09-21 19:39:10 -0500

    test network

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,34 @@
 <template>
  
   <div>
-     <span>here is a thing</span>
+      <button @click="right()">go team</button>
+     <span>{{this.showme}}</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  data(){
+    return {
+        sledPosition:0,
+        newb:["this thing"],
+        question: 'https://demo.jaredfuller.com/hg/index.php?dates=2022-09-10&type=event,movie,attraction',
+        showme: null
+    }
+  },
+  methods: {
+    right(){
+      this.question = 'https://demo.jaredfuller.com/hg/index.php?dates=2022-09-30&type=event,movie,attraction'
+      this.$fetch()
+      //console.log('lets see this work for once')
+    }
+  },
+  async fetch() {
+      var data = await fetch(this.question).then(res => res.json())
+     // console.log(data)
+     this.showme = data
+      //this.newb = data
+  },
 }
 </script>
 <style lang="scss" scoped>
