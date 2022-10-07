3a2f73b1bffb3fad0230ebcbfc8e69b4d3316ebc
Damien <34070001+DefinitelyNotGitHubOfficial@users.noreply.github.com>
2022-10-07 16:17:59 -0500

    idk lol

--- pages/attractions/_id/index.vue
+++ pages/attractions/_id/index.vue
@@ -4,7 +4,9 @@
             <!-- {{this.$route.params.id}} -->
             <input placeholder="put in date yyyy-mm-dd" id="in">
             <button @click="change()">fetch data</button>
-            {{this.thing}}
+            <br /><br />   
+            <div class="block">{{this.thing}}</div> 
+            
             
         </div>
 </template>
@@ -19,6 +21,76 @@ export default{
         // console.log(this.$route.params)
         // console.log('xxxxxxxxxxxx',this.$route.params.id)
         // console.log(`https://demo.jaredfuller.com/hg/index.php${this.$route.params.id}`)
+
+        const month = [
+            {"01":'Jan'},
+            {"02":'Feb'},
+            {"03":'Mar'},
+            {"04":'Apr'},
+            {"05":'May'},
+            {"06":'Jun'},
+            {"07":'Jul'},
+            {"08":'Aug'},
+            {"09":'Sep'},
+            {"10":'Oct'},
+            {"11":'Nov'},
+           { "12":'Dec'}
+        ]
+
+       
+       var pre = month.map(month => {
+           return month["01"]
+       })
+       .filter(x=>x)
+       .join()
+    
+       // "returns Jan"
+
+
+
+        //var result = pre
+
+       // var result = month.map
+
+        // case '01':
+        //             return 'Jan ' + date.slice(8,10)
+        //             break;
+        //         case '02':
+        //             return 'Feb ' + date.slice(8,10)
+        //             break;
+        //         case '03':
+        //             return 'Mar ' + date.slice(8,10)
+        //             break;
+        //         case '04':
+        //             return 'Apr ' + date.slice(8,10)
+        //             break;
+        //         case '05':
+        //             return 'May ' + date.slice(8,10)
+        //             break;
+        //         case '06':
+        //             return 'Jun ' + date.slice(8,10)
+        //             break;
+        //         case '07':
+        //             return 'Jul ' + date.slice(8,10)
+        //             break;
+        //         case '08':
+        //             return 'Aug ' + date.slice(8,10)
+        //             break;
+        //         case '09':
+        //             return 'Sep ' + date.slice(8,10)
+        //             break;
+        //         case '10':
+        //             return 'Oct ' + date.slice(8,10)
+        //             break;
+        //         case '11':
+        //             return 'Nov ' + date.slice(8,10)
+        //             break;
+        //         case '12':
+        //             return 'Dec ' + date.slice(8,10)
+
+
+
+
         
     },
     methods: {
@@ -48,5 +120,9 @@ export default{
 div {
     color:#fff;
     padding:20px;
+}
+.block {
+    background-color: $purple;
+    padding:15px;
 }
 </style>
