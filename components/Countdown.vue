b2012c0fea30e250d466c7a188b8003847412bdc
Damien <34070001+DefinitelyNotGitHubOfficial@users.noreply.github.com>
2023-04-20 07:49:26 -0500

    added init for countdown

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -14,6 +14,26 @@
     mounted(){
         var countDownDate = new Date("Oct 10, 2023 0:0:0").getTime()
 
+
+
+
+
+        //sloppy for now but it gets the job done.
+        // Get today's date and time
+        var now = new Date().getTime();
+            
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+            
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+        // Output the result in an element with id="demo"
+        document.getElementById("cake").innerHTML = days + " days " + hours + " hours "
+        + minutes + " minutes ";
         var x = setInterval(function() {
 
         // Get today's date and time
