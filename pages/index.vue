d70749af98f06d37016d5113681346bb0d8a02c7
Damien <34070001+DefinitelyNotGitHubOfficial@users.noreply.github.com>
2023-09-23 01:15:37 -0500

    small update to get the latest from the list

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
           </a>
         </div> -->
         <Graveyard />
-        <News />
+        <News /> 
         <!-- <HauntedDuluth /> -->
         <GrimCities />  
         <!-- <Twitter /> -->
